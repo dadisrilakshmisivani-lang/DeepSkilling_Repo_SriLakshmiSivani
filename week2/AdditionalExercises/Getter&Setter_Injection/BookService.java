06a71dd7c50a0d09c8ad905107049e3dd1ebd71c
Dadi Sri Lakshmi Sivani <dadisrilakshmisivani@gmail.com>
2026-06-30 14:19:49 +0530

    Add files via upload

---
+++ week2/AdditionalExercises/Getter&Setter_Injection/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor Injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Constructor Injection Executed");
+    }
+
+    public void showBook() {
+        bookRepository.displayBook();
+    }
+}
