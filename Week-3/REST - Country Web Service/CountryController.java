a9af065066de26aa7e483b47d07175a2ca6e540e
Dadi Sri Lakshmi Sivani <dadisrilakshmisivani@gmail.com>
2026-07-08 19:44:16 +0530

    Add files via upload

---
+++ Week-3/REST - Country Web Service/CountryController.java
@@ -0,0 +1,29 @@
+package SpringWebProject;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CountryController {
+
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("beans.xml");
+
+
+        Country country =
+                context.getBean("india", Country.class);
+
+
+        return country;
+    }
+
+}
