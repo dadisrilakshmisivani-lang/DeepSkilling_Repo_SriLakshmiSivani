73d7c6ee6bc251b03d46e72a9429179e02b8e03d
Dadi Sri Lakshmi Sivani <dadisrilakshmisivani@gmail.com>
2026-07-13 16:26:23 +0530

    Add files via upload

---
+++ Week-5/Exercise-2/StudentApp/App1.jsx
@@ -0,0 +1,15 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+function App1() {
+  return (
+    <>
+      <Home />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default App1;
